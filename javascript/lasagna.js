63ad3f362230a8a87d567cb7f73bcb868e039acb
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2022-09-27 15:55:41 +0600

    solved annalyns infiltration

--- javascript/lasagna.js
+++ javascript/lasagna.js
@@ -1,14 +1,14 @@
-export const EXPECTED_MINUTES_IN_OVEN = 40;
-export const PREPARATION_MINUTES_PER_LAYER = 2;
+export const EXPECTED_MINUTES_IN_OVEN = 40
+export const PREPARATION_MINUTES_PER_LAYER = 2
 
 export function remainingMinutesInOven(actualMinutesInOven) {
-  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
 }
 
 export function preparationTimeInMinutes(numberOfLayers) {
-  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER
 }
 
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
 }
